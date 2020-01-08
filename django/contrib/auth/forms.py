c85bdb99ce55814dd58b7ed467d102a94f9145f3
Daniel Ristic-Petrovic <d.risticpe@gmail.com>
2020-01-08 10:04:53 -0700

    Fix for CVE-2019-19844

--- django/contrib/auth/forms.py
+++ django/contrib/auth/forms.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+import unicodedata
+
 from django import forms
 from django.forms.util import flatatt
 from django.template import loader
@@ -7,6 +9,7 @@ from django.utils.datastructures import SortedDict
 from django.utils.html import format_html, format_html_join
 from django.utils.http import int_to_base36
 from django.utils.safestring import mark_safe
+from django.utils.six import PY3
 from django.utils.text import capfirst
 from django.utils.translation import ugettext, ugettext_lazy as _
 
@@ -20,6 +23,20 @@ from django.contrib.sites.models import get_current_site
 UNMASKED_DIGITS_TO_SHOW = 6
 
 mask_password = lambda p: "%s%s" % (p[:UNMASKED_DIGITS_TO_SHOW], "*" * max(len(p) - UNMASKED_DIGITS_TO_SHOW, 0))
+
+
+def _unicode_ci_compare(s1, s2):
+    """
+    Perform case-insensitive comparison of two identifiers, using the
+    recommended algorithm from Unicode Technical Report 36, section
+    2.11.2(B)(2).
+    """
+    normalized1 = unicodedata.normalize('NFKC', s1)
+    normalized2 = unicodedata.normalize('NFKC', s2)
+    if PY3:
+        return normalized1.casefold() == normalized2.casefold()
+    # lower() is the best alternative available on Python 2.
+    return normalized1.lower() == normalized2.lower()
 
 
 class ReadOnlyPasswordHashWidget(forms.Widget):
@@ -217,7 +234,11 @@ class PasswordResetForm(forms.Form):
         """
         UserModel = get_user_model()
         email = self.cleaned_data["email"]
-        self.users_cache = UserModel._default_manager.filter(email__iexact=email)
+        self.users_cache = [
+            u for u in
+            UserModel._default_manager.filter(email__iexact=email)
+            if _unicode_ci_compare(email, u.email)
+        ]
         if not len(self.users_cache):
             raise forms.ValidationError(self.error_messages['unknown'])
         if not any(user.is_active for user in self.users_cache):
